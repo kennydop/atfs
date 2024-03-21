ac8fc4618f146d24a1b8b7102fd994180b132f73
Patrick Offei Danso <dansooffeipatrick@gmail.com>
2024-03-21 21:39:57 +0000

    feat: base sign up and sign in

--- app.js
+++ app.js
@@ -1,41 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: "shhhh, very secret",
+  })
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Session-persisted message middleware
+app.use(function (req, res, next) {
+  var err = req.session.error;
+  var msg = req.session.success;
+  delete req.session.error;
+  delete req.session.success;
+  res.locals.message = "";
+  if (err) res.locals.message = '<p class="msg error">' + err + "</p>";
+  if (msg) res.locals.message = '<p class="msg success">' + msg + "</p>";
+  next();
+});
+
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,79 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const authUtils = require("../controllers/auth");
+
+router.get("/login", function (req, res) {
+  res.render("login");
+});
+
+router.post("/login", function (req, res, next) {
+  authUtils.authenticate(
+    req.body.username,
+    req.body.password,
+    function (err, user) {
+      if (err) return next(err);
+      if (user) {
+        // Regenerate session when signing in
+        // to prevent fixation
+        req.session.regenerate(function () {
+          // Store the user's primary key
+          // in the session store to be retrieved,
+          // or in this case the entire user object
+          req.session.user = user;
+          req.session.success =
+            "Authenticated as " +
+            user.name +
+            ' click to <a href="/logout">logout</a>. ' +
+            ' You may now access <a href="/restricted">/restricted</a>.';
+          res.redirect("back");
+        });
+      } else {
+        req.session.error =
+          "Authentication failed, please check your " +
+          " username and password." +
+          ' (use "tj" and "foobar")';
+        res.redirect("/login");
+      }
+    }
+  );
+});
+
+router.get("/signup", function (req, res) {
+  res.render("signup");
+});
+
+router.post("/signup", function (req, res, next) {
+  authUtils.registerUser(
+    req.body.username,
+    req.body.password,
+    function (err, user) {
+      if (err) return next(err);
+      if (user) {
+        req.session.regenerate(function () {
+          req.session.user = user;
+          req.session.success = "Registration successful! You can now log in.";
+          res.redirect("/");
+        });
+      } else {
+        req.session.error = "Registration failed. Please try again.";
+        res.redirect("/signup");
+      }
+    }
+  );
+});
+
+router.get("/logout", function (req, res) {
+  // destroy the user's session to log them out
+  // will be re-created on next request
+  req.session.destroy(function () {
+    res.redirect("/");
+  });
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", authUtils.restrict, function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
